16bde1fd28f838f61a2a625dfcb1d5beb61b047f
Matheus <matheusbaron10@gmail.com>
2021-03-06 12:46:28 -0300

    Feat: global styles created

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+  :root {
+    --background: #F8F8FB;
+    --title: #283E58;
+    --description: #A6ACBE;
+    --shadow: #EDEDF6;
+
+    --white: #FFFFFF;
+    --orange: #FF7A00;
+    --white-purple: #9C69E2;
+    --purple: #6F52ED;
+    --red: #FF4C61;
+    --green: #33D69F;
+    --yellow: #FFB800;
+
+    --dark-background: #06090F;
+    --dark-description: #C9D1D9;
+    --dark-card: #161B22;
+  }
+
+  html {
+    font-size: 62.5%
+  } 
+
+  * {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    background: var(--background);
+  }
+`
